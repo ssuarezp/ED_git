8976f0b5149d02cd2104dfb707c35dd0974d032e
ssuarezp <ssuarezp@unal.edu.co>
2025-04-29 21:13:41 -0500

    s

---
+++ eclipse/src/main/java/Principal.java
@@ -0,0 +1,20 @@
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		
+		int[] a= {1,2,3};
+		
+		float[] b = new float[5];
+		
+		for (int i=0;i<a.length;i++) {
+			
+			System.out.println(a[i]);
+			
+		}
+		
+		
+		
+	}
+
+}
